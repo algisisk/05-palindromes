d3106df22ae3a41889d5e62936beb7ed9c971ffa
algisisk <kylian.algisi-sisowath@edu.esiee.fr>
2024-11-08 19:07:57 +0100

    envoi

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ Ce module contient une fonction pour vérifier si un mot est palindrome,
 ainsi qu'une fonction principale pour afficher les mots palindromes dans la liste de test.
 """
 
+import string
+from unidecode import unidecode
+
 #### Fonction secondaire
 # La fonction ispalindrome() permet de renvoyer si le mot en paramètre est un palindrome
 def ispalindrome(p):
@@ -15,18 +18,30 @@ def ispalindrome(p):
     Retourne:
     bool: True si le mot est un palindrome, False sinon
     """
-    #return p == "".join(list(p).slit().reverse())
-    return p == p[::-1]
+    # unicode() enlève les accents des lettres
+    # replace() enlève les espaces
+    # translate(...) enlève les ponctuations
+    # str[::-1] renverse la chaîne de caractère
+    p_normal = (unidecode(p)
+                .lower()
+                .replace(" ", "")
+                .translate(str.maketrans('', '', string.punctuation)))
+    p_invers = (unidecode(p)
+                .lower().replace(" ", "")
+                .translate(str.maketrans('', '', string.punctuation)))[::-1]
 
+    return p_normal == p_invers
 
 #### Fonction principale
-# Fonction principale qui fait tourner le code en testant si les mots mis dans la liste sont palindromes
+# Fonction principale qui fait tourner le code
+# en testant si les mots mis dans la liste sont palindromes
 
 def main():
     """
     Affiche les mots qui sont des palindromes
     """
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique",
+              "deifie","Oh ! cela te perd, répéta l'écho"]:
         print(s, ispalindrome(s))
 
 
