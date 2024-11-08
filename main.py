957e983501c8189dafc78da4134220417bdfaae8
algisisk <kylian.algisi-sisowath@edu.esiee.fr>
2024-11-08 14:46:45 +0000

    first version

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+"""
+Ce module contient une fonction pour vérifier si un mot est palindrome,
+ainsi qu'une fonction principale pour afficher les mots palindromes dans la liste de test.
+"""
+
 #### Fonction secondaire
-
-
+# La fonction ispalindrome() permet de renvoyer si le mot en paramètre est un palindrome
 def ispalindrome(p):
+    """
+    Détermine si un mot est palindrome
 
-    # votre code ici
-    
-    return False
+    Paramètre:
+    p (str): Le mot à vérifier
+
+    Retourne:
+    bool: True si le mot est un palindrome, False sinon
+    """
+    #return p == "".join(list(p).slit().reverse())
+    return p == p[::-1]
+
 
 #### Fonction principale
-
+# Fonction principale qui fait tourner le code en testant si les mots mis dans la liste sont palindromes
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Affiche les mots qui sont des palindromes
+    """
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
